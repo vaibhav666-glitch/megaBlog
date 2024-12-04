64b3203ae8be204456095ab8d1237f79d0e6458d
vaibhav <vaibhavning@gmail.com>
2024-12-04 20:13:18 +0530

    Add postForm

--- src/component/Header/LogoutBtn.jsx
+++ src/component/Header/LogoutBtn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import authService from '../appwrite/auth'
-import { logout } from '../store/authSlice'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/authSlice'
 
 export default function LogoutBtn() {
     const dispatch = useDispatch()
